988222ddc811c3ffc0f9535c72bf79cac086dbf6
abiralshrestha <108796378+abiralshrestha@users.noreply.github.com>
2022-07-07 21:52:36 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-function seer() {
-  const rh = document.getElementById("tgl-hrmj") /*hiragana romaji toggle*/
-  const rk = document.getElementById("tgl-krmj") /*katakana romaji toggle*/
-  const th = document.getElementById("h-tbl") /*hiragana table*/
-  const tk = document.getElementById("k-tbl") /*katakana table*/
-
-  if (rh.checked == true) {
-    th.classList.add("hide-even")
+function toggle(input, id) {
+  if (input.checked == true) {
+    document.getElementById(id).classList.add("hide-even")
   } else {
-    th.classList.remove("hide-even")
-  }
-  if (rk.checked == true) {
-    tk.classList.add("hide-even")
-  } else {
-    tk.classList.remove("hide-even")
+    document.getElementById(id).classList.remove("hide-even")
   }
 }
