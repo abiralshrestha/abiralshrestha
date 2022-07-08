09110226a789d90913c2f76974e00bb871fbda37
abiralshrestha <108796378+abiralshrestha@users.noreply.github.com>
2022-07-08 09:53:45 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggle(input, id) {
-  if (input.checked == true) {
-    document.getElementById(id).classList.add("hide-even")
-  } else {
-    document.getElementById(id).classList.remove("hide-even")
-  }
-}
+    if (input.checked == true) {
+      document.getElementById(id).classList.add("hide-even")
+    } else {
+      document.getElementById(id).classList.remove("hide-even")
+    }
+  } 
